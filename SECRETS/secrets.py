044c7e2b5df8dae2940cd35753ba23b78b43807b
Varun Rao <Varun.Rao095@gmail.com>
2018-06-04 01:57:05 +0000

    Added gitignore to uninclude SECRETS

--- SECRETS/secrets.py
+++
@@ -1,8 +0,0 @@
-URL = "https://{}/v{}/articles?source={}&sortBy=top&apiKey={}"
-DOMAIN_NAME = "newsapi.org"
-API_KEY = "4672835cdc4146128410598825f2e5a2"
-VERSION_NO = 1
-NEWSPAPER = "the-times-of-india"
-EMAIL = "info@kkny.org"
-PASSWORD = "kkny1101"
-SMTP_SERVER = "smtpout.secureserver.net"
