bee08c0a50fd57508f9a8b60271f0ae84065a20c
Varun Rao <Varun.Rao095@gmail.com>
2018-06-12 02:12:58 +0000

    Seperated previous events (now on the about us page) from future events (which remains on the home page)

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $("#populate_carousel").slick({variableWidth: true, centerMode: true, slidesToShow:3, slidesToScroll: 3, dots: true, autoplay: true, autoplaySpeed: 1500});
+    $("#upcoming_events_carousel").slick({variableWidth: true, slidesToShow:2, slidesToScroll: 2, dots: true, autoplay: true, autoplaySpeed: 1500});
 })
