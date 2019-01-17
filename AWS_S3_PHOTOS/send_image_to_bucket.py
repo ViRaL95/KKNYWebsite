7566471e17fa8796819a72e95acbad493341ee55
Varun Rao <Varun.Rao095@gmail.com>
2019-01-17 22:51:04 +0000

    fix: Remove AWS secret keys

--- AWS_S3_PHOTOS/send_image_to_bucket.py
+++ AWS_S3_PHOTOS/send_image_to_bucket.py
@@ -1,29 +1,27 @@
 import boto3
 import uuid
 import datetime
+import sys
+sys.path.insert(0, '/home/KKNY/KKNY_Website_Production_Environment')
+from SECRETS import secrets
 
 class Photo_Send_s3:
     def __init__(self):
-        self.access_key = 'AKIAJW7PRZDO57ND665A'
-        self.AWSSecretKey = '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'
+        self.access_key = secrets.AWS_ACCESS_KEY
+        self.AWSSecretKey = secrets.AWS_SECRET_KEY
         self.allowed_image_extensions = ["png", "jpg", "jpeg", "gif"]
 
     def send_photo_to_s3(self, photos, user_info, event_info):
-        print(datetime.datetime.now()) 
         session = boto3.Session(aws_access_key_id=self.access_key, aws_secret_access_key=self.AWSSecretKey)
-        print(datetime.datetime.now()) 
         s3 = session.client('s3')
-        print(datetime.datetime.now()) 
         for photo in photos:
             #Ensure that the image contains an extension
-            print(datetime.datetime.now())
             if "." not in photo.filename:
                 return {"success": False, "message": "Please only include images with extensions"}
             #Get extension from filename provided by user. extension SHOULD BE .png, .jpg, .jpeg, or .gif
             image_extension = photo.filename.split(".")[-1]
             if image_extension.lower() not in self.allowed_image_extensions:
                 return {"success": False, "message": "Please only include images with .png, .jpg, .jpeg or .gif extension"}
-            print(photo.stream) 
             #Generate a random file name. Why generate a random file name? What if two users submit two different images with the same file name? S3 will remove the first image.
             random_file_name = str(uuid.uuid4())
 
