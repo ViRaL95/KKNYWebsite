2d7bed7f6b358be9208729bd574aa587b11e102d
Varun Rao <vrao0919@gmail.com>
2020-06-23 12:32:27 -0400

    clean: Removed newline, changed camelcase to snake style naming

--- DATABASE/renderEvents.py
+++ DATABASE/renderEvents.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 
+
 class Events:
     def __init__(self):
         self.client = MongoClient('localhost', 27017)
         self.database = self.client.KKNY
 
-    def retrieveEvents(self):
+    def retrieve_events(self):
         event_names = self.database['event_names']
         return event_names.find({}) 
